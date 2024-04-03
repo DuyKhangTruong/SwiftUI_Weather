813e3010715fdda18d88c432648d1ba8d852dda4
Khang Nguyen <36774417+DuyKhangTruong@users.noreply.github.com>
2024-04-03 00:57:21 -0700

    Initial Commit

---
+++ SwiftUI_Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI_Weather
+//
+//  Created by Duy Khang Nguyen Truong on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
